10c09553d777ed521e03eb4c8c4365b32e74427f
Lyoubomir Katzarov <lyoubomir.katzarov@yahoo.com>
2021-05-24 13:47:37 +0300

    Format all ts files

--- src/app.ts
+++ src/app.ts
@@ -9,27 +9,27 @@ import passport from "passport";
 
 app.use(express.json());
 
-import path from 'path';
+import path from "path";
 import dotenv from "dotenv";
-dotenv.config({ path: path.resolve(__dirname, '../.env') })
+dotenv.config({ path: path.resolve(__dirname, "../.env") });
 
 import db from "./models/index";
 
 if (process.argv[2] === "dev") {
-  db.sequelize.sync({ force: true }).then(() => {
-    console.log("Drop and re-sync db.");
-  });
+    db.sequelize.sync({ force: true }).then(() => {
+        console.log("Drop and re-sync db.");
+    });
 }
 
 app.use(
-  session({
-    secret: process.env.SESSION_SECRET!,
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-      maxAge: 1000 * 60 * 60 * 24, // Equals 1 day (1 day * 24 hr/1 day * 60 min/1 hr * 60 sec/1 min * 1000 ms / 1 sec)
-    },
-  })
+    session({
+        secret: process.env.SESSION_SECRET!,
+        resave: false,
+        saveUninitialized: false,
+        cookie: {
+            maxAge: 1000 * 60 * 60 * 24 // Equals 1 day (1 day * 24 hr/1 day * 60 min/1 hr * 60 sec/1 min * 1000 ms / 1 sec)
+        }
+    })
 );
 
 require("./config/passport");
@@ -38,14 +38,14 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use((req, _res, next) => {
-  console.log(req.session);
-  console.log(req.user);
+    console.log(req.session);
+    console.log(req.user);
 
-  if (req.user) {
-    //@ts-ignore
-    console.log(req.user.email);
-  }
-  next();
+    if (req.user) {
+        //@ts-ignore
+        console.log(req.user.email);
+    }
+    next();
 });
 
 import userRoutes from "./routes/UserRoutes";
@@ -54,5 +54,5 @@ app.use("/api", userRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
+    console.log(`Server is running on port ${PORT}.`);
 });
--- src/utils/passwordUtils.ts
+++ src/utils/passwordUtils.ts
@@ -1,16 +1,17 @@
 import crypto from "crypto";
 
-export function hashPassword(password: string, salt = crypto.randomBytes(32).toString("hex")): {salt: string, phash: string} {
-  const phash = crypto
-    .pbkdf2Sync(password, salt, 10000, 64, "sha512")
-    .toString("hex");
+export function hashPassword(
+    password: string,
+    salt = crypto.randomBytes(32).toString("hex")
+): { salt: string; phash: string } {
+    const phash = crypto.pbkdf2Sync(password, salt, 10000, 64, "sha512").toString("hex");
 
-  return {
-    salt: salt,
-    phash: phash,
-  };
+    return {
+        salt: salt,
+        phash: phash
+    };
 }
 
 export function isSamePassword(candidate: string, phash: string, salt: string): boolean {
-  return phash === hashPassword(candidate, salt).phash;
+    return phash === hashPassword(candidate, salt).phash;
 }
