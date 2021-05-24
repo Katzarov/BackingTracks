c11a4929a6c04f1852b98b5e32abe81d4d4d053b
Lyoubomir Katzarov <lyoubomir.katzarov@yahoo.com>
2021-05-24 13:27:34 +0300

    Convert utils to use ts and modules

---
+++ src/utils/passwordUtils.ts
@@ -0,0 +1,16 @@
+import crypto from "crypto";
+
+export function hashPassword(password: string, salt = crypto.randomBytes(32).toString("hex")): {salt: string, phash: string} {
+  const phash = crypto
+    .pbkdf2Sync(password, salt, 10000, 64, "sha512")
+    .toString("hex");
+
+  return {
+    salt: salt,
+    phash: phash,
+  };
+}
+
+export function isSamePassword(candidate: string, phash: string, salt: string): boolean {
+  return phash === hashPassword(candidate, salt).phash;
+}
