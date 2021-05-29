a0848a4b4a2a98b29e324f7429099623ce331009
Lyoubomir Katzarov <lyoubomir.katzarov@yahoo.com>
2021-05-29 15:34:57 +0300

    Add port env var to db connection init

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,10 +1,14 @@
-const Sequelize = require("sequelize");
+import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
-    dialect: "postgres"
-});
-
-console.log("HIT");
+const sequelize = new Sequelize(
+    process.env.DB_NAME!,
+    process.env.DB_USER!,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT as unknown as number,
+        dialect: "postgres"
+    }
+);
 
 export default sequelize;
